bb8e1434e222e54edddba7a14548577381c38d69
mahla-mohamadi <mahlamhmd78@gmail.com>
2023-12-09 21:42:07 +0330

    ssssss

--- header.php
+++ header.php
@@ -59,8 +59,7 @@
 					</div>
 				</div>
 				<div class="headerWoocomerceItemsPart">
-				<?php echo do_shortcode('[fibosearch]'); ?>
-				<?php 
+				<?php echo do_shortcode('[fibosearch]'); 
 				if ( is_user_logged_in() ) {
 				?>
 				<div class="headerMyAcountIcon">
@@ -81,14 +80,15 @@
 				<?php
 				 } else { ?>
 					<a href="#" class="kadinerUsersLogout"><span>ورود / عضویت</span><svg width="20" height="20" viewBox="8 8 50 50" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path style="fill:none" d="M0 0h1280v800H0z" transform="translate(-1216 -192)"/><path d="M50 46.5c-7.2 0-13-5.8-13-13s5.8-13 13-13 13 5.8 13 13-5.8 13-13 13Zm0-22c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9Z" style="fill-rule:nonzero" transform="translate(-1216 -192) matrix(.9393 0 0 .9393 1200.662 180.725)"/><path d="M34.036 58.5V67H30.4v-8.5c0-7.182 8.818-12.727 20-12.727s20 5.545 20 12.727V67h-3.636v-8.5c0-4.909-7.455-9.091-16.364-9.091S34.036 53.591 34.036 58.5Z" style="fill-rule:nonzero" transform="translate(-1216 -192) translate(1195.93 178.799) scale(1.03318)"/></svg></a>
-					<?php } ?>
+				<?php } ?>
+				</div>
 			</div>
 		</div>
 	</div>
 
 
 
-	
+
 	<div class="mobileHeaderContainer">
 		<div class="mobileHeader mainView">
 			<a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 773.74 217.85"><defs><linearGradient id="linear-gradient" x1="554.63" y1="-5.65" x2="720.73" y2="244.45" gradientUnits="userSpaceOnUse"><stop offset=".09" stop-color="#5b5b5b"/><stop offset=".99" stop-color="#1a1e1c"/></linearGradient><linearGradient id="linear-gradient-2" x1="567.07" y1="-13.91" x2="733.18" y2="236.18" xlink:href="#linear-gradient"/><linearGradient id="linear-gradient-3" x1="622.2" y1="-50.53" x2="788.31" y2="199.57" xlink:href="#linear-gradient"/><linearGradient id="linear-gradient-4" x1="534" y1="-23.71" x2="699.3" y2="143.89" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".95" stop-color="#fff" stop-opacity="0"/></linearGradient><style>.cls-7{fill:#2f322e}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M773.74 53.55v23.76c0 1.35 0 2.67-.13 4v-1.49c-.53 15.47-7.7 25.17-24.58 25.62h-32.69a55.19 55.19 0 0 0-55.18 55.18v31.84c-.08 17.62-9.83 24.8-25.61 25.34h.79c-1.11.06-2.24.1-3.4.1h-23.51a53.54 53.54 0 0 1-53.54-53.54V53.55A53.54 53.54 0 0 1 609.43 0H720.2a53.54 53.54 0 0 1 53.54 53.55Z" style="fill:#fdcf00"/><path d="M675.47 123.51a55 55 0 0 0-14.31 37.07v12.09a49.29 49.29 0 0 1-45.4-41.88 6.38 6.38 0 0 1 6.32-7.28Z" style="fill:url(#linear-gradient)"/><path d="M773.58 79.78v1.49l-.06 5.93a44.49 44.49 0 0 1-13.06 31.05L676 202.67a51.39 51.39 0 0 1-35.79 15h-4.65c15.78-.54 25.53-7.72 25.61-25.34v-31.75a55.19 55.19 0 0 1 55.18-55.18H749c16.88-.4 24.05-10.15 24.58-25.62Z" style="fill:#feea8e"/><path d="M644.3 81.91a5.57 5.57 0 0 1-5.57-5.57 13.64 13.64 0 1 0-27.27 0 5.57 5.57 0 0 1-11.14 0 24.77 24.77 0 0 1 49.54 0 5.57 5.57 0 0 1-5.56 5.57Z" style="fill:url(#linear-gradient-2)"/><path d="M723.75 81.91a5.57 5.57 0 0 1-5.57-5.57 13.64 13.64 0 1 0-27.28 0 5.57 5.57 0 1 1-11.13 0 24.77 24.77 0 1 1 49.54 0 5.57 5.57 0 0 1-5.56 5.57Z" style="fill:url(#linear-gradient-3)"/><path d="M773.74 53.55v23.76c0 1.35 0 2.67-.13 4v-1.49c-.53 15.47-7.7 25.17-24.58 25.62h-32.69a55.19 55.19 0 0 0-55.18 55.18v31.84c-.08 17.62-9.83 24.8-25.61 25.34h.79c-1.11.06-2.24.1-3.4.1h-23.51a53.54 53.54 0 0 1-53.54-53.54V53.55A53.54 53.54 0 0 1 609.43 0H720.2a53.54 53.54 0 0 1 53.54 53.55Z" style="fill:url(#linear-gradient-4)"/><path class="cls-7" d="M497.05 125.83c.37 21.38-17.59 38.88-39 38.88h-88.16a43.16 43.16 0 0 1-43.13-43.11V50.76h29.54v70.84a13.58 13.58 0 0 0 13.58 13.57h88.95a8.69 8.69 0 1 0 0-17.38l-47-.23a18.5 18.5 0 0 1-13-5.41l-11.6-11.6A18.47 18.47 0 0 1 384.5 78l20.82-34.7 25.33 15.2-17.86 29.75h46a38.27 38.27 0 0 1 38.26 37.58Zm-234.26 39.09H240.4v-29.54h22.39a17.88 17.88 0 1 0 0-35.75H240.4l7.09-29.54h15.3a47.42 47.42 0 1 1 0 94.83Zm-68.21-94.09V104a26.76 26.76 0 1 1-53.51 0V70.83h-29.54v5.27l-.28 28.49a26.76 26.76 0 1 1-53.51 0V71.38H28.2v28.79A68.55 68.55 0 0 1 8 149l-8 9.4 22 19.67 7.15-8.54a98.64 98.64 0 0 0 17.27-23.48 56.18 56.18 0 0 0 79.95-3.92A56.27 56.27 0 0 0 224.12 104V70.83Z"/><circle class="cls-7" cx="178.91" cy="182.01" r="13.93"/><circle class="cls-7" cx="126.65" cy="48.31" r="14.17"/><circle class="cls-7" cx="209.46" cy="181.24" r="13.93"/></g></g></svg></a>
